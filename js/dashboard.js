50ac248d1523f9cdd15f5f6437de0b6e3f12afba
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-03-08 00:19:34 +0600

    search issues js complete

--- js/dashboard.js
+++ js/dashboard.js
@@ -98,3 +98,19 @@ function showIssues(issues) {
     container.appendChild(card);
   });
 }
+
+// Search
+async function searchIssue() {
+  const text = document.getElementById("search").value;
+
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`,
+  );
+
+  const data = await res.json();
+
+  showIssues(data.data);
+
+  document.getElementById("issueCount").innerText =
+    data.data.length + " Issues";
+}
