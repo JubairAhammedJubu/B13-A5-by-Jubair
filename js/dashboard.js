20f57fc83e0ea5caeba40d2d455e4f693a0dfc43
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-03-07 21:39:18 +0600

    show issues js complete

--- js/dashboard.js
+++ js/dashboard.js
@@ -24,3 +24,77 @@ async function loadIssues(type = "all") {
   showIssues(issues);
   document.getElementById("issueCount").innerText = `${issues.length} Issues`;
 }
+
+
+// showIssues
+
+function showIssues(issues) {
+  container.innerHTML = "";
+
+  issues.forEach((issue) => {
+    const border =
+      issue.status === "open" ? "border-emerald-600" : "border-purple-500";
+
+    const logo =
+      issue.status === "open"
+        ? `<img src="assets/Open-Status.png" alt="Open">`
+        : `<img src="assets/Closed- Status .png" alt="Closed" >`;
+
+    const priorityColor = {
+      high: "bg-red-100 text-red-500",
+      medium: "bg-yellow-100 text-yellow-600",
+      low: "bg-gray-200 text-gray-600",
+    };
+
+    const card = document.createElement("div");
+
+    card.className = `bg-white rounded-md shadow border-t-4 ${border} p-4 flex flex-col h-full space-y-3 `;
+
+    card.innerHTML = `
+    
+    <div class="flex justify-between items-center">
+        ${logo} 
+        <p class="text-xs px-3 py-1 rounded-full ${
+          priorityColor[issue.priority]
+        }">
+          ${issue.priority.toUpperCase()}
+        </p>
+    </div>
+    <h3 class="font-semibold text-lg leading-snug">
+      ${issue.title}
+    </h3>
+
+
+    <p class="text-sm text-gray-500">
+      ${issue.description}
+    </p>
+
+    <div class="grow"></div> <!-- takes all the empty space -->
+    <div class="flex gap-2">
+
+      <span class="text-xs px-2 py-1 bg-red-100 text-red-500 rounded-full">
+        BUG
+      </span>
+
+      <span class="text-xs px-2 py-1 bg-yellow-100 text-yellow-600 rounded-full">
+        HELP WANTED
+      </span>
+
+    </div>
+
+    <hr class="border border-zinc-200">
+
+    <div class="text-sm text-gray-500 ">
+      #${issue.id} by ${issue.author}
+    </div>
+
+    <div class="text-xs text-gray-400">
+      ${new Date(issue.createdAt).toLocaleDateString()}
+    </div>
+
+    `;
+
+    card.addEventListener("click", () => openModal(issue));
+    container.appendChild(card);
+  });
+}
