9e1934f4d44fdb7c12879101fd45b3b4f061c613
JubairAhammedJubu <0432410005101112@uits.edu.bd>
2026-03-08 18:14:18 +0600

    complete

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,9 @@
 const container = document.getElementById("container");
 const searchInput = document.getElementById("search");
 const searchBtn = document.getElementById("searchBtn");
-const API = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+const allBtn = document.getElementById("allBtn");
+const openBtn = document.getElementById("openBtn");
+const closedBtn = document.getElementById("closedBtn");
 
 searchBtn.addEventListener("click", searchIssue);
 
@@ -10,6 +12,29 @@ loadIssues();
 
 // Load issues
 async function loadIssues(type = "all") {
+  const buttons = [allBtn, openBtn, closedBtn];
+  // Reset all buttons
+  buttons.forEach((btn) => {
+    btn.classList.remove("bg-blue-700", "text-white");
+    btn.classList.add("bg-white", "hover:border-blue-700");
+  });
+
+  // active button
+  if (type === "all") {
+    allBtn.classList.remove("bg-white", "hover:border-blue-700");
+    allBtn.classList.add("bg-blue-700", "text-white");
+  }
+
+  if (type === "open") {
+    openBtn.classList.remove("bg-white", "hover:border-blue-700");
+    openBtn.classList.add("bg-blue-700", "text-white");
+  }
+
+  if (type === "closed") {
+    closedBtn.classList.remove("bg-white", "hover:border-blue-700");
+    closedBtn.classList.add("bg-blue-700", "text-white");
+  }
+
   const res = await fetch(
     "https://phi-lab-server.vercel.app/api/v1/lab/issues",
   );
@@ -25,9 +50,7 @@ async function loadIssues(type = "all") {
   document.getElementById("issueCount").innerText = `${issues.length} Issues`;
 }
 
-
 // showIssues
-
 function showIssues(issues) {
   container.innerHTML = "";
 
@@ -48,11 +71,11 @@ function showIssues(issues) {
 
     const card = document.createElement("div");
 
-    card.className = `bg-white rounded-md shadow border-t-4 ${border} p-4 flex flex-col h-full space-y-3 `;
+    card.className = `cursor-pointer hover:-translate-y-1 transition duration-350 bg-white rounded-md shadow border-t-4 ${border} py-4 flex flex-col h-full `;
 
     card.innerHTML = `
     
-    <div class="flex justify-between items-center">
+    <div class="flex justify-between items-center px-4">
         ${logo} 
         <p class="text-xs px-3 py-1 rounded-full ${
           priorityColor[issue.priority]
@@ -60,36 +83,35 @@ function showIssues(issues) {
           ${issue.priority.toUpperCase()}
         </p>
     </div>
-    <h3 class="font-semibold text-lg leading-snug">
+    <h3 class="font-semibold text-lg leading-snug mt-3  px-4">
       ${issue.title}
     </h3>
 
-
-    <p class="text-sm text-gray-500">
-      ${issue.description}
+    <div class="grow"></div> <!-- takes all the empty space -->
+    <p class="text-sm text-slate-500 my-3  px-4">
+      ${issue.description.slice(0, 80) + "..."}
     </p>
 
-    <div class="grow"></div> <!-- takes all the empty space -->
-    <div class="flex gap-2">
+    <div class="flex gap-2  px-4">
 
-      <span class="text-xs px-2 py-1 bg-red-100 text-red-500 rounded-full">
-        BUG
+      <span class="text-xs px-2 py-1 border border-red-200 bg-red-100 text-red-500 rounded-full">
+      <i class="fa-solid fa-bug"></i> BUG
       </span>
 
-      <span class="text-xs px-2 py-1 bg-yellow-100 text-yellow-600 rounded-full">
-        HELP WANTED
+      <span class="text-xs px-2 py-1 border border-amber-300 bg-yellow-100 text-amber-600 rounded-full">
+       <i class="fa-solid fa-life-ring"></i> HELP WANTED
       </span>
 
     </div>
-
-    <hr class="border border-zinc-200">
-
-    <div class="text-sm text-gray-500 ">
+  
+    <hr class="border-t-2 border-gray-200 my-3">
+    
+    <div class="text-sm text-slate-500  px-4">
       #${issue.id} by ${issue.author}
     </div>
 
-    <div class="text-xs text-gray-400">
-      ${new Date(issue.createdAt).toLocaleDateString()}
+    <div class="text-xs text-slate-500 px-4">
+      ${new Date(issue.createdAt).toLocaleDateString("en-US")}
     </div>
 
     `;
@@ -144,7 +166,6 @@ function openModal(issue) {
   status.className = `text-white text-sm px-4 py-1 rounded-full font-medium ${s.color}`;
 
   document.getElementById("modalName").innerText = s.text;
-
 
   //priratory
   const priority = document.getElementById("modalPriority");
